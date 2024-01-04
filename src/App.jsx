1da23b25b8f25d79fdb33652af71eaba24cad059
JohnWesley2112 <johnwesley14307@gmail.com>
2024-01-04 13:36:44 +0530

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App main-container">
+      <nav>
+        <img src={require('./logo-1080x1080.jpg')} alt=" " />
+        <div className="links">
+          <h4>contact</h4>
+          <h4>login</h4>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
